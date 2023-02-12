22fadbacb924a595372ce57efbbb4c0a0de3465b
David Malchin <malchin459@gmail.com>
2023-02-12 12:14:10 +0200

    Format assembler

--- assembler.c
+++ assembler.c
@@ -2,22 +2,21 @@
 #include <stdlib.h>
 #include "preassembler.h"
 
-int main(int argc, char**argv){
+int main(int argc, char **argv) {
 	int i;
-	FILE *file,*temp;
-	if(argc==1){
-		fprintf(stderr,"error no files mentioned\n");
-		exit(1);	
+	FILE *file, *temp;
+	if (argc == 1) {
+		fprintf(stderr, "Error: no files mentioned\n");
+		return 1;
 	}
-	for(i=0; i+1<argc; i++){
-		temp=fopen(argv[i+1],"r");
-		if(temp==NULL){
-			fprintf(stderr,"error in opening %s.as\n",argv[i+1]);
+	for (i = 0; i+1 < argc; i++) {
+		temp = fopen(argv[i+1], "r");
+		if (temp == NULL) {
+			fprintf(stderr, "error in opening %s.as\n", argv[i+1]);
 			exit(1);
 		}
-		file=preassembler(temp,argv[i+1]);
+		file = preassembler(temp, argv[i+1]);
 		fclose(temp);
-		
 	}
 	return 0;
 }
