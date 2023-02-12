440bad1088ccbd72cf9751ca82034747ae11642e
David Malchin <malchin459@gmail.com>
2023-02-12 15:46:06 +0200

    Remove unused function definition

--- preassembler.h
+++ preassembler.h
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 void sort(char *data,char *sours,int length, FILE *fp);
-int isEmpty(char *str);
 int skipwhitespace(char line[], int i);
 int countnonwhitespace(char line[], int i, int count);
 FILE *preassembler(FILE *fd, char *nameF);
