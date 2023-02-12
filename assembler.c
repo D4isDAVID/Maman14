ddcaa00164d5cb36b70002432a1c0c06797ee5f4
David Malchin <malchin459@gmail.com>
2023-02-12 12:15:36 +0200

    Use shorter `i` rather than `i+1`

--- assembler.c
+++ assembler.c
@@ -8,13 +8,13 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "Error: no files mentioned\n");
 		return 1;
 	}
-	for (i = 0; i+1 < argc; i++) {
-		temp = fopen(argv[i+1], "r");
+	for (i = 1; i < argc; i++) {
+		temp = fopen(argv[i], "r");
 		if (temp == NULL) {
-			fprintf(stderr, "error in opening %s.as\n", argv[i+1]);
+			fprintf(stderr, "error in opening %s.as\n", argv[i]);
 			return 1;
 		}
-		file = preassembler(temp, argv[i+1]);
+		file = preassembler(temp, argv[i]);
 		fclose(temp);
 	}
 	return 0;
