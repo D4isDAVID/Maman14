f72ff3a8f94b4cd1345da536da99ece0cea31dba
David Malchin <malchin459@gmail.com>
2023-02-12 12:43:30 +0200

    Format mlist

--- mlist.c
+++ mlist.c
@@ -8,44 +8,44 @@ struct mlist {
 	char *content;
 };
 
-static struct mlist *head=NULL;
+static struct mlist *head = NULL;
 
-void addNode(char *name,char *content){
+void addNode(char *name, char *content) {
 	struct mlist *temp;
-	if(head==NULL){
-		head=(struct mlist *)malloc(sizeof(struct mlist));
-		head->name=(char *)malloc(sizeof(char)*strlen(name));
-		head->name=strcpy(head->name,name);
-		head->content=(char *)malloc(sizeof(char)*strlen(content));
-		head->content=strcpy(head->content,content);
-		head->next=NULL;
-	}
-	else{
-		temp=(struct mlist *)malloc(sizeof(struct mlist));
-		temp->name=(char *)malloc(sizeof(char)*strlen(name));
-		temp->name=strcpy(temp->name,name);
-		temp->content=(char *)malloc(sizeof(char)*strlen(content));
-		temp->content=strcpy(temp->content,content);
-		temp->next=head;
-		head=temp;
+	if (head == NULL) {
+		head = (struct mlist *) malloc(sizeof(struct mlist));
+		head->name = (char *) malloc(sizeof(char) * strlen(name));
+		head->name = strcpy(head->name, name);
+		head->content = (char *) malloc(sizeof(char) * strlen(content));
+		head->content = strcpy(head->content, content);
+		head->next = NULL;
+	} else {
+		temp = (struct mlist *) malloc(sizeof(struct mlist));
+		temp->name = (char *) malloc(sizeof(char) * strlen(name));
+		temp->name = strcpy(temp->name, name);
+		temp->content = (char *)malloc(sizeof(char) * strlen(content));
+		temp->content = strcpy(temp->content, content);
+		temp->next = head;
+		head = temp;
 	}
 }
-char *lookup(char *s){
+
+char *lookup(char *s) {
 	struct mlist *np;
-	np=head;
-	while(np!=NULL){
-		if(strcmp(s,np->name)==0)
+	np = head;
+	while (np != NULL) {
+		if (strcmp(s, np->name) == 0)
 			return np->content;
-		np=np->next;
-	} 
+		np = np->next;
+	}
 	return NULL;
-
 }
-void clear(){
+
+void clear() {
 	struct mlist *temp;
-	while(head!=NULL){
-		temp=head->next;
+	while (head != NULL) {
+		temp = head->next;
 		free(head);
-		head=temp;
+		head = temp;
 	}
 }
