c878962bcbad3d30b3844dba1cedee91da78895d
David Malchin <malchin459@gmail.com>
2023-02-14 12:33:48 +0200

    Add include guards

--- mlist.h
+++ mlist.h
@@ -1,3 +1,8 @@
+#ifndef MLIST_H
+#define MLIST_H
+
 void mlist_add(char *name,char *content);
 char *mlist_lookup(char *s);
 void mlist_clear();
+
+#endif
--- parser.h
+++ parser.h
@@ -1,4 +1,9 @@
+#ifndef PARSER_H
+#define PARSER_H
+
 #define MAX_LINE_LENGTH 80 /* max line length without terminator and possible final newline */
 
 int skipwhitespace(char [], int *);
 int countnonwhitespace(char [], int *);
+
+#endif
